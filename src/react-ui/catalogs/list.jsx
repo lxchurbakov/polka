bff0738f249131019913add6af55b50f75edae2b
lxchurbakov <lxchurbakov@gmail.com>
2024-02-09 08:36:35 +0300

    Create list catalogs and edit catalog pages

---
+++ src/react-ui/catalogs/list.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { Clickable, Base, Text } from 'libs/atoms';
+
+export default ({ router }) => {
+    const create = React.useCallback(() => {
+        const id = /* create new */ '100';
+
+        router.navigate('catalogs/edit', { id });
+    }, []);
+
+    return (
+        <Base p="32px">
+            <Text mb="12px" size="32px" weight="600">
+                Ваши каталоги
+            </Text>
+
+            <Text mb="24px" size="16px" weight="400">
+                Все документы организованы по каталогам.
+            </Text>
+
+            <Clickable onClick={create} background="#006699" p="6px 10px" radius="4px">
+                <Text size="14px" weight="600" color="white">Добавить каталог</Text>
+            </Clickable>
+        </Base>
+    );
+};
