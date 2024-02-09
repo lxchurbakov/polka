7dff52cc7a44c733cd78d971a3f7272fdcf46fca
lxchurbakov <lxchurbakov@gmail.com>
2024-02-09 08:36:18 +0300

    Draft useRouter hook

--- src/react-ui/index.jsx
+++ src/react-ui/index.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 
-import { Clickable, Base, Text } from 'libs/atoms';
-import IPC from 'plugins/communication/renderer';
+import ListCatalogs from './catalogs/list';
+import EditCatalog from './catalogs/edit';
 
-const ipc = new IPC();
+const useRouter = () => {
+    const [page, setPage] = React.useState('catalogs/list');
+    const [params, setParams] = React.useState({});
+
+    const navigate = React.useCallback((page, params) => {
+        setPage(page);
+        setParams(params);
+    }, [setPage, setParams]);
+
+    return React.useMemo(() => ({
+        page, params, navigate
+    }), [page, params, navigate]);
+};
 
 export default () => {
-    const [value, setValue] = React.useState(0);
+    const router = useRouter();
 
-    const handleClick = React.useCallback(() => {
-        ipc.send({ type: 'ping', data: Math.random() });
-    }, []);
+    if (router.page === 'catalogs/list') {
+        return (
+            <ListCatalogs router={router} />
+        );
+    }
 
-    React.useEffect(() => {
-        return ipc.onMessage.on(({ type, data }) => {
-            if (type === 'pong') {
-                setValue(data);
-            }
-        });
-    }, []);
-
-    return (
-        <Base>
-            <Text mb="12px" size="16px" weight="300">Click to test IPC</Text>
-
-            <Clickable onClick={handleClick} background="#006699" p="6px 8px" radius="2px">
-                <Text size="14px" weight="600" color="white">Test IPC</Text>
-            </Clickable>
-
-            <Text>Response: {value}</Text>
-        </Base>
-    );
+    if (router.page === 'catalogs/edit') {
+        return (
+            <EditCatalog router={router} />
+        );
+    }
 };
